7bc463eec4394889ecc8ce50fad7a32ec41b4507
Giovanni Inarejo <OmegaPoring@hotmail.com>
2023-05-01 21:19:14 -0400

    "Se agrega archivo drilling.py a Sesion 04"

---
+++ Sesion 04/Drilling.py
@@ -0,0 +1,9 @@
+#Regitro de teres personas#
+
+persona_1 = {"nombre": "Juan", "apellido": "Perez", "telefono": "987654321"}
+persona_2 = {"nombre": "Pedro", "apellido": "Ramirez", "telefono": "987321654"}
+persona_3 = {"nombre": "Marco", "apellido": "Polo", "telefono": "987456123"}
+
+lista = [persona_1, persona_2, persona_3]
+
+print(lista)
